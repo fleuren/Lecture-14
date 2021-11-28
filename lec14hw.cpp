1f18485815c2dfc232d8b69224a864ec660e025a
fleuren <91404708+fleuren@users.noreply.github.com>
2021-11-28 19:29:56 +0400

    Add files via upload

---
+++ lec14hw.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+//Input taken from one array
+/*
+	array<int, 10> numbers = {};
+	cout << "Enter integers " << ":" << endl;
+	for(int i = 0; i < 10; i++){
+	cin >> numbers[i];
+}
+	sort(numbers.begin(), numbers.end()); // sorting algorithm code
+
+//Outputs
+	cout << "The smallest number is: ";
+	cout << numbers.at(0) << endl;
+	cout << "...and the largest number is: ";
+	cout << numbers.at(9) << endl;
+
+return 0;
+}
+*/
+
+//Input taken from two arrays:
+	//Smallest number
+/*
+	array<int, 10> numx = {};
+	cout << "This program will display the smallest number from your inputted range." << endl;
+	cout << "For the smallest number output, please enter your desired integers " << ":" << endl;
+	for(int i = 0; i < 10; i++){
+	cin >> numx[i];
+}
+	sort(numx.begin(), numx.end()); // sorting algorithm code
+
+//Output: Smallest number	
+	cout << "The smallest number is: ";
+	cout << numx.at(0) << endl;
+	
+	//Largest number
+	array<int, 10> numy = {};
+	cout << "This program will display the smallest number from your inputted range." << endl;
+	cout << "For the smallest number output, please enter your desired integers " << ":" << endl;
+	for(int j = 0; j < 10; j++){
+	cin >> numy[j];
+}
+	sort(numy.begin(), numy.end()); // sorting algorithm code
+
+//Output: Largest number
+	cout << "The largest number is: ";
+	cout << numy.at(9) << endl;
+return 0;
+}	
+*/
